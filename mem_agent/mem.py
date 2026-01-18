30d043d3ca59aab3e7a2c3f9f11dab0cca1fd95a
Muskan Chauhan <158822241+Muskanchauhan29@users.noreply.github.com>
2026-01-18 22:02:21 +0530

    Add files via upload

--- mem_agent/mem.py
+++ mem_agent/mem.py
@@ -1,7 +1,12 @@
+from dotenv import load_dotenv
 from mem0 import Memory
 import os
+from openai import OpenAI
 
-OPENAI_API_KEY = os.getenv("OPENAPI_API_KEY")
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 config = {
     "version": "v1.1",
@@ -10,16 +15,41 @@ config = {
         "config": {"api_key": OPENAI_API_KEY, "model": "text-embedding-3-small"}
     },
     "llm": {
-        "provider": "openai",
-        "config": {"api_key": OPENAI_API_KEY, "model": "text-embedding-3-small"} 
-    },
-    "vector_store": {
-        "provider": "qdrant",
-        "config": {
-            "host": "localhost",
-            "port": 6333
-        }
+    "provider": "openai",
+    "config": {
+        "api_key": OPENAI_API_KEY,
+        "model": "gpt-4.1-mini"
     }
+},
+
+   "vector_store": {
+    "provider": "qdrant",
+    "config": {
+        "path": "./qdrant_data"
+    }
+}
 }
 
 mem_client = Memory.from_config(config)
+
+user_query = input("> ")
+
+response = client.chat.completions.create(
+    model="gpt-4.1-mini",
+    messages=[
+        {"role":"user", "content": user_query}
+    ]
+)
+
+ai_response = response.choices[0].message.content
+
+print("AI", ai_response)
+mem_client.add(
+    user_id="mussu",
+    messages=[
+        {"role":"user", "content":user_query},
+        {"role":"assistant", "content":ai_response}
+    ]
+)
+
+print("memory has been saved!!")
